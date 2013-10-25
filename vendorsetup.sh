f1f6f8e6d75898b7022dea1978c9b87470e8ad86
yajnab <yajnab@gmail.com>
2013-10-25 22:31:02 +0530

    Fixes for CWM

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_golfu-userdebug
-add_lunch_combo full_golfu
+add_lunch_combo cm_golfu-userdebug
+add_lunch_combo cm_golfu
